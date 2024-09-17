df27e6c2e3f25bd3dfe8640d6c13e04c2b9140e4
Eli Bendersky <eliben@gmail.com>
2024-09-17 14:25:40 -0700

    Sketch out more of the http infra, with comments

--- part4kv/kvservice/kvservice.go
+++ part4kv/kvservice/kvservice.go
@@ -19,6 +19,12 @@ type KVService struct {
 	srv *http.Server
 }
 
+// New creates a new KVService
+//
+//   - id: this service's ID within its Raft cluster
+//   - peerIds: the IDs of the other Raft peers in the cluster
+//   - readyChan: notification channel that has to be closed when the Raft
+//     cluster is ready (all peers are up and connected to each other).
 func New(id int, peerIds []int, readyChan <-chan any) *KVService {
 	commitChan := make(chan raft.CommitEntry)
 
@@ -42,17 +48,16 @@ func (kvs *KVService) GetRaftListenAddr() net.Addr {
 	return kvs.rs.GetListenAddr()
 }
 
-// TODO: Serve method that serves REST on HTTP (with a port it's given)
-// Requires clean shutdown with http.Server.Shutdown
-// Serve doesn't block, but it gets all the processes started.
-// ... a Shutdown method cleans everything up, calls DisconnectAll and Shutdown
-// on the underlying rs, shuts down http server, etc.
-
+// ServeHTTP starts serving the KV REST API on the given TCP port. This
+// function does not block; it fires up the HTTP server and returns. To properly
+// shut down the server, call the Shutdown method.
 func (kvs *KVService) ServeHTTP(port string) {
 	if kvs.srv != nil {
 		panic("ServeHTTP called with existing server")
 	}
 	mux := http.NewServeMux()
+	mux.HandleFunc("POST /get/", kvs.handleGet)
+	mux.HandleFunc("POST /put/", kvs.handlePut)
 
 	kvs.srv = &http.Server{
 		Addr:    ":" + port,
@@ -66,6 +71,19 @@ func (kvs *KVService) ServeHTTP(port string) {
 	}()
 }
 
+// Shutdown performs a proper shutdown of the service; it disconnects from
+// all Raft peers, shuts down the Raft RPC server, and shuts down the main
+// HTTP service. It only returns once shutdown is complete.
 func (kvs *KVService) Shutdown() error {
+	kvs.rs.DisconnectAll()
+	kvs.rs.Shutdown()
 	return kvs.srv.Shutdown(context.Background())
+}
+
+func (kvs *KVService) handleGet(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func (kvs *KVService) handlePut(w http.ResponseWriter, req *http.Request) {
+
 }
