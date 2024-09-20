bba8be21a7223e523b4ec56168686e7182dfb9c1
Eli Bendersky <eliben@gmail.com>
2024-09-20 09:22:43 -0700

    Refactor sending request

--- part4kv/kvclient/kvclient.go
+++ part4kv/kvclient/kvclient.go
@@ -27,41 +27,17 @@ func New(serviceAddrs []string) *KVClient {
 }
 
 func (c *KVClient) Put(key string, value string) error {
-	body := new(bytes.Buffer)
-	enc := json.NewEncoder(body)
-	if err := enc.Encode(api.PutRequest{Key: key, Value: value}); err != nil {
-		return err
-	}
-
 	path := fmt.Sprintf("http://%s/put/", c.addrs[0])
-	req, err := http.NewRequest(http.MethodPost, path, body)
+	putReq := api.PutRequest{
+		Key:   key,
+		Value: value,
+	}
+	c.clientlog("sending %v to %v", putReq, path)
+	putResp, err := sendJSONRequest[api.PutResponse](path, putReq)
 	if err != nil {
 		return err
 	}
-	req.Header.Add("Content-Type", "application/json")
-
-	client := &http.Client{
-		Timeout: 5 * time.Second,
-	}
-	c.clientlog("sending PUT request to %v: %v=%v", c.addrs[0], key, value)
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-
-	//bb, err := io.ReadAll(resp.Body)
-	//if err != nil {
-	//panic(err)
-	//}
-	//fmt.Println("YY", string(bb))
-
-	var pr api.PutResponse
-	dec := json.NewDecoder(resp.Body)
-	if err := dec.Decode(&pr); err != nil {
-		return err
-	}
-
-	fmt.Println(pr)
+	c.clientlog("received response %v", putResp)
 	return nil
 }
 
@@ -71,4 +47,34 @@ func (c *KVClient) clientlog(format string, args ...any) {
 		format = "[client] " + format
 		log.Printf(format, args...)
 	}
+}
+
+func sendJSONRequest[ResponseT any](path string, data any) (*ResponseT, error) {
+	body := new(bytes.Buffer)
+	enc := json.NewEncoder(body)
+	if err := enc.Encode(data); err != nil {
+		return nil, fmt.Errorf("JSON-encoding request data: %w", err)
+	}
+
+	req, err := http.NewRequest(http.MethodPost, path, body)
+	if err != nil {
+		return nil, fmt.Errorf("creating HTTP request: %w", err)
+	}
+	req.Header.Add("Content-Type", "application/json")
+
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("issuing HTTP request: %w", err)
+	}
+
+	var responseData ResponseT
+	dec := json.NewDecoder(resp.Body)
+	if err := dec.Decode(&responseData); err != nil {
+		return nil, fmt.Errorf("JSON-decoding response data: %w", err)
+	}
+	return &responseData, nil
 }
