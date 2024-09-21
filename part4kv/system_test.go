cb3fb44f9edbee12892ba3c1ad5dc93645460ad9
Eli Bendersky <eliben@gmail.com>
2024-09-21 08:58:58 -0700

    Adding tests

--- part4kv/system_test.go
+++ part4kv/system_test.go
@@ -31,3 +31,42 @@ func TestBasicPutGetSingleClient(t *testing.T) {
 	}
 	sleepMs(80)
 }
+
+func TestPutPrevValue(t *testing.T) {
+	h := NewHarness(t, 3)
+	defer h.Shutdown()
+	h.CheckSingleLeader()
+
+	c1 := kvclient.New(h.kvServiceAddrs)
+	// Make sure we get the expected found/prev values before and after Put
+	prev, found := h.CheckPut(c1, "llave", "cosa")
+	if found || prev != "" {
+		t.Errorf(`got found=%v, prev=%v, want false/""`, found, prev)
+	}
+
+	prev, found = h.CheckPut(c1, "llave", "frodo")
+	if !found || prev != "cosa" {
+		t.Errorf(`got found=%v, prev=%v, want true/"cosa"`, found, prev)
+	}
+
+	// A different key...
+	prev, found = h.CheckPut(c1, "mafteah", "davar")
+	if found || prev != "" {
+		t.Errorf(`got found=%v, prev=%v, want false/""`, found, prev)
+	}
+}
+
+func TestBasicPutGetDifferentClients(t *testing.T) {
+	h := NewHarness(t, 3)
+	defer h.Shutdown()
+	h.CheckSingleLeader()
+
+	c1 := kvclient.New(h.kvServiceAddrs)
+	h.CheckPut(c1, "k", "v")
+
+	c2 := kvclient.New(h.kvServiceAddrs)
+	if v := h.CheckGetFound(c2, "k"); v != "v" {
+		t.Errorf(`got %v, want "v"`, v)
+	}
+	sleepMs(80)
+}
